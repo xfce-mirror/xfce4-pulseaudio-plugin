2b4f80bb9126c123936f305067fd28bf4a920417
Błażej Szczygieł <mumei6102@gmail.com>
2023-03-26 15:38:07 +0200

    Fix changing default sink and source devices
    Change sink/source only, don't do anything else. Now it should be the
    same as changing it from pactl or pavucontrol.
    
    Closes #78

--- panel-plugin/pulseaudio-volume.c
+++ panel-plugin/pulseaudio-volume.c
@@ -57,10 +57,6 @@ static void                 pulseaudio_volume_get_source_output_info_cb (pa_cont
                                                                          const pa_source_output_info *i,
                                                                          int                   eol,
                                                                          void                 *userdata);
-static void                 pulseaudio_volume_move_sink_input           (pa_context           *context,
-                                                                         const                 pa_sink_input_info *i,
-                                                                         int                   eol,
-                                                                         void                 *userdata);
 
 
 
@@ -1015,54 +1011,6 @@ pulseaudio_volume_get_default_input (PulseaudioVolume *volume)
 
 
 
-static void
-pulseaudio_volume_default_sink_changed_info_cb (pa_context         *context,
-                                                const pa_sink_info *i,
-                                                int                 eol,
-                                                void               *userdata)
-{
-  PulseaudioVolume *volume = PULSEAUDIO_VOLUME (userdata);
-
-  if (i == NULL)
-    return;
-
-  pa_context_move_sink_input_by_index (context, volume->sink_index, i->index, NULL, NULL);
-  volume->sink_index = (guint)i->index;
-
-  pa_context_get_sink_input_info_list (volume->pa_context, pulseaudio_volume_move_sink_input, volume);
-}
-
-
-
-static void
-pulseaudio_volume_default_sink_changed (pa_context *context,
-                                        int         success,
-                                        void       *userdata)
-{
-  PulseaudioVolume *volume = PULSEAUDIO_VOLUME (userdata);
-
-  if (success)
-    pa_context_get_sink_info_by_name (volume->pa_context, volume->default_sink_name, pulseaudio_volume_default_sink_changed_info_cb, volume);
-}
-
-
-
-static void
-pulseaudio_volume_move_sink_input (pa_context               *context,
-                                   const pa_sink_input_info *i,
-                                   int                       eol,
-                                   void                     *userdata)
-{
-  PulseaudioVolume *volume = PULSEAUDIO_VOLUME (userdata);
-
-  if (i == NULL) return;
-  if (eol > 0) return;
-
-  pa_context_move_sink_input_by_name (context, i->index, volume->default_sink_name, NULL, NULL);
-}
-
-
-
 void
 pulseaudio_volume_set_default_output (PulseaudioVolume *volume,
                                       const gchar      *name,
@@ -1071,41 +1019,16 @@ pulseaudio_volume_set_default_output (PulseaudioVolume *volume,
   if (g_strcmp0(name, volume->default_sink_name) == 0)
     return;
 
-  g_free (volume->default_sink_name);
-  volume->default_sink_name = g_strdup (name);
-
   if (make_default)
-    pa_context_set_default_sink (volume->pa_context, name, pulseaudio_volume_default_sink_changed, volume);
-}
-
-
-
-static void
-pulseaudio_volume_default_source_changed_info_cb (pa_context         *context,
-                                                  const pa_source_info *i,
-                                                  int                 eol,
-                                                  void               *userdata)
-{
-  PulseaudioVolume *volume = PULSEAUDIO_VOLUME (userdata);
-
-  if (i == NULL)
-    return;
-
-  pa_context_move_source_output_by_index (context, volume->source_index, i->index, NULL, NULL);
-  volume->source_index = (guint)i->index;
-}
-
-
-
-static void
-pulseaudio_volume_default_source_changed (pa_context *context,
-                                          int         success,
-                                          void       *userdata)
-{
-  PulseaudioVolume *volume = PULSEAUDIO_VOLUME (userdata);
-
-  if (success)
-    pa_context_get_source_info_by_name (volume->pa_context, volume->default_source_name, pulseaudio_volume_default_source_changed_info_cb, volume);
+    {
+      /* "default_sink_name" will be updated in "else" called from "pulseaudio_volume_subscribe_cb" callback */
+      pa_context_set_default_sink (volume->pa_context, name, NULL, volume);
+    }
+  else
+    {
+      g_free (volume->default_sink_name);
+      volume->default_sink_name = g_strdup (name);
+    }
 }
 
 
@@ -1118,11 +1041,16 @@ pulseaudio_volume_set_default_input (PulseaudioVolume *volume,
   if (g_strcmp0 (name, volume->default_source_name) == 0)
     return;
 
-  g_free (volume->default_source_name);
-  volume->default_source_name = g_strdup (name);
-
   if (make_default)
-    pa_context_set_default_source (volume->pa_context, name, pulseaudio_volume_default_source_changed, volume);
+    {
+      /* "default_source_name" will be updated in "else" called from "pulseaudio_volume_subscribe_cb" callback */
+      pa_context_set_default_source (volume->pa_context, name, NULL, volume);
+    }
+  else
+    {
+      g_free (volume->default_source_name);
+      volume->default_source_name = g_strdup (name);
+    }
 }
 
 
