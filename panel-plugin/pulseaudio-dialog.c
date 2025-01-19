a65a273b84c9a75342f09c47e4d5119af1b12be3
Gaël Bonithon <gael@xfce.org>
2025-01-19 13:25:36 +0100

    Fix libxfce4panel include
    Include guards are added to headers other than libxfce4panel.h in
    libxfce4panel 4.21.0, so no headers other than this one should be
    included.
    
    Code preceding this patch can still be built by linking against
    libxfce4panel >= 4.21.0 by adding -DLIBXFCE4PANEL_COMPILATION to CFLAGS.
    
    Related: xfce/xfce4-panel!162

--- panel-plugin/pulseaudio-dialog.c
+++ panel-plugin/pulseaudio-dialog.c
@@ -36,7 +36,7 @@
 #include <glib.h>
 #include <gtk/gtk.h>
 
-#include <libxfce4panel/xfce-panel-plugin.h>
+#include <libxfce4panel/libxfce4panel.h>
 #include <libxfce4util/libxfce4util.h>
 #include <libxfce4ui/libxfce4ui.h>
 
