3654b71834bd5e6d8589764d738abb0de7f38552
Bobby Rong <rjl931189261@126.com>
2025-03-16 11:33:18 +0800

    Add meson build

--- panel-plugin/pulseaudio-plugin.c
+++ panel-plugin/pulseaudio-plugin.c
@@ -28,6 +28,9 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+#ifdef HAVE_XFCE_REVISION_H
+#include "xfce-revision.h"
+#endif
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif
@@ -267,7 +270,7 @@ pulseaudio_plugin_show_about (XfcePanelPlugin *plugin)
   gtk_show_about_dialog (NULL,
                          "logo-icon-name", "xfce4-pulseaudio-plugin",
                          "license",        xfce_get_license_text (XFCE_LICENSE_TEXT_GPL),
-                         "version",        PACKAGE_VERSION,
+                         "version",        VERSION_FULL,
                          "program-name",   PACKAGE_NAME,
                          "comments",       _("Adjust the audio volume of the PulseAudio sound system"),
                          "website",        "https://docs.xfce.org/panel-plugins/xfce4-pulseaudio-plugin",
