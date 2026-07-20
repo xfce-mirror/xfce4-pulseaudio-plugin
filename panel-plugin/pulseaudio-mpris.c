e053b77fc3e0f81a2f03b3190b0039918d609f72
Gaël Bonithon <gael@xfce.org>
2026-07-20 11:56:13 +0200

    cppcheck: Fix knownConditionTrueFalse

--- panel-plugin/pulseaudio-mpris.c
+++ panel-plugin/pulseaudio-mpris.c
@@ -138,8 +138,7 @@ pulseaudio_mpris_get_available_players (PulseaudioMpris *mpris)
                                    &error);
   if (error)
     {
-      g_warning ("Could not get a list of names registered on the session bus, %s",
-                 error ? error->message : "no error given");
+      g_warning ("Could not get a list of names registered on the session bus, %s", error->message);
       g_clear_error (&error);
       return NULL;
     }
